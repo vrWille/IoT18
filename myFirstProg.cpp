26a0edeacfb5d05998bd16df1a3e689a7ff0e8b0
vrwille <tomas.willemsen@hotmail.se>
2018-09-09 10:54:53 +0200

    test file

---
+++ myFirstProg.cpp
@@ -0,0 +1,24 @@
+/****************************************
+* Filename: myFirstProg.cpp
+* 
+* author: wille
+* created: 2018- 09-09
+* notes:
+* 
+* desc:
+* 
+* ver: 2018- 09-09 first version
+* 
+* 
+* *************************************/
+#include <iostream>
+
+int main(){
+
+    //print to screen
+    std::cout << "Hello World!";
+
+    return 0;
+}
+
+
